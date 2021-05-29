f2f8a19964fcd1284e27236f9761c581af88c523
xD3m1anx <9249210@gmail.com>
2021-05-30 01:51:09 +0300

    Create new local lib for easy using shortly keys. ESP8266 IO pin names.

---
+++ src/esp8266gpio.h
@@ -0,0 +1,41 @@
+#ifndef ESP8266GPIO_H
+#define ESP8266GPIO_H
+
+#define GPIO0   0
+#define GPIO1   1
+#define GPIO2   2
+#define GPIO3   3
+#define GPIO4   4
+#define GPIO5   5
+#define GPIO6   6
+#define GPIO7   7
+#define GPIO8   8
+#define GPIO9   9
+#define GPIO10  10
+#define GPIO11  11
+#define GPIO12  12
+#define GPIO13  13
+#define GPIO14  14
+#define GPIO15  15
+#define GPIO16  16
+
+#define D0      GPIO16
+#define D1      GPIO5
+#define D2      GPIO4
+#define D3      GPIO0
+#define D4      GPIO2
+#define D5      GPIO14
+#define D6      GPIO12
+#define D7      GPIO13
+#define D8      GPIO15
+#define RX      GPIO3
+#define TX      GPIO1
+#define A0      ADC
+#define S3      GPIO10
+#define S2      GPIO9
+#define S1      GPIO8
+#define S0      GPIO11
+#define SK      GPIO7
+#define SC      GPIO6
+
+#endif
